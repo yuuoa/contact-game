bccc604438c82b99afc9572cd30c0aa45e2dcc5f
yuuoa <alwanandika@gmail.com>
2022-02-19 18:04:09 +0700

    add main menu

---
+++ Assets/Flask.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flask : MonoBehaviour
+{
+
+    private float HealAmount = 10f;
+    private Transform player;
+    private float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>().Health;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(+HealAmount);
+        PlayerPrefs.SetFloat("Health", health);
+        Destroy(gameObject);
+    }
+}
